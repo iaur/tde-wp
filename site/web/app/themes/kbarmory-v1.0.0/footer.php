77763ad4c1de96153edce2f1e71f0b3dd040ecba
Case <bernie.lu@caseMacBook-Pro.local>
2020-11-02 16:45:13 +0800

    codes deployment

---
+++ site/web/app/themes/kbarmory-v1.0.0/footer.php
@@ -0,0 +1,38 @@
+                    
+                        
+
+                
+                  
+                    <div class="container-footer">
+                        <footer class="site-footer"> 
+                            <p><?php bloginfo('name')?> - &copy;&copy;  - <?php echo date('Y'); ?> | Powered by kbarmory</p>
+                        </footer>
+                    </div> 
+            
+        </div><!-- /container -->
+        
+
+    </body>
+
+    <script>
+        var balls = document.getElementsByClassName("ball");
+        document.onmousemove = function(){
+            var x = event.clientX * 100 / window.innerWidth + "%";
+            var y = event.clientY * 100 / window.innerHeight + "%";
+            //event.clientX => get the horizontal coordinate of the mouse
+            //event.clientY => get the Vertical coordinate of the mouse
+            //window.innerWidth => get the browser width
+            //window.innerHeight => get the browser height
+
+            for(var i=0;i<2;i++){
+            balls[i].style.left = x;
+            balls[i].style.top = y;
+            balls[i].style.transform = "translate(-"+x+",-"+y+")";
+            }
+        }
+    </script>
+
+
+<?php wp_footer(); ?>
+
+</html>
---
+++ site/web/app/themes/kbarmory-v1.0.0/header.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+
+<html <?php language_attributes(); ?>>
+
+    <head>
+        <meta charset="<? bloginfo('charset'); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+        <title><?php bloginfo('name'); ?></title>
+        <?php wp_head(); ?>
+    </head>
+
+    <body <?php body_class(); ?> >
+
+
+        <div class="container"><!-- container -->
+        
+                <header class="site-header">
+                    
+                    <a href="<?php echo home_url(); ?>" class="logo-eye">
+                        <img src="http://thedailyexploits.local/app/uploads/2020/10/LogoMakr-3Q4c31.png" alt="eye" height="60" width="200">
+                    </a>
+
+                    <div class="mobile-flex">
+
+                        <div class="hamburger">
+                            <input class="menu-btn" type="checkbox" id="menu-btn" />
+                            <label class="menu-icon" for="menu-btn"><span class="nav-icon"></span></label>
+                                
+                            <div class="site-nav"><!-- header navigation menu-->     
+                                <?php
+                                    $args = array(
+                                        'theme_location' => 'primary'
+                                    );
+                                    ?>
+                                <?php wp_nav_menu( $args); ?>
+                            </div><!-- /header navigation menu-->
+                        </div>
+                        
+                        <!-- header-search (id:hs) -->
+                        <div class="search-container">
+                            <?php get_search_form(); ?>      
+                        </div>
+                        <!-- /header-search -->
+                    </div>
+                    
+                </header>
+
+        
+        
+
+        
