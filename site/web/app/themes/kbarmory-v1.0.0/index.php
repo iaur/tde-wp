ee2bc7215bc89ea3f4332e4172e3e140f65eef3b
Case <bernie.lu@caseMacBook-Pro.local>
2020-11-06 17:02:03 +0800

    mobile padding top

--- site/web/app/themes/kbarmory-v1.0.0/index.php
+++ site/web/app/themes/kbarmory-v1.0.0/index.php
@@ -39,63 +39,65 @@ get_header(); ?>
             </div>
         <!-- /Stucture: Main-Column -->
 
-        <div class="tree">
-                
+        <div class="tree">     
             <div class="section-name">
                 <h3>Library</h3>
             </div>
-            <ul>   
-                <?php 
-                    $get_parent_cats = array(
-                        'parent' => '0' //get top level categories only
-                    ); 
 
-                    $all_categories = get_categories( $get_parent_cats );//get parent categories 
-
-                    foreach( $all_categories as $single_category ){
-                        //for each category, get the ID
-                        $catID = $single_category->cat_ID;
-
-                        echo '<li><a href=" ' . get_category_link( $catID ) . ' ">' . $single_category->name . '</a>'; //category name & link
-                        echo '<ul class="post-title">';
-
-                       
-                        wp_reset_postdata();
-
-                        echo '</ul>';
-                        $get_children_cats = array(
-                            'child_of' => $catID //get children of this parent using the catID variable from earlier
-                        );
-
-                        $child_cats = get_categories( $get_children_cats );//get children of parent category
-                        echo '<ul class="children">';
+            <div class="tree-container">
+                <ul>   
+                    <?php 
+                        $get_parent_cats = array(
+                            'parent' => '0' //get top level categories only
+                        ); 
+                        
+                        $all_categories = get_categories( $get_parent_cats );//get parent categories 
+                        
+                        foreach( $all_categories as $single_category ){
+                            //for each category, get the ID
+                            $catID = $single_category->cat_ID;
+                            
+                            echo '<li><a href=" ' . get_category_link( $catID ) . ' ">' . $single_category->name . '</a>'; //category name & link
+                            echo '<ul class="post-title">';
+                            
+                            
+                            wp_reset_postdata();
+                            
+                            echo '</ul>';
+                            $get_children_cats = array(
+                                'child_of' => $catID //get children of this parent using the catID variable from earlier
+                            );
+                            
+                            $child_cats = get_categories( $get_children_cats );//get children of parent category
+                            echo '<ul class="children">';
                             foreach( $child_cats as $child_cat ){
                                 //for each child category, get the ID
                                 $childID = $child_cat->cat_ID;
-
+                                
                                 //for each child category, give us the link and name
                                 echo '<a href=" ' . get_category_link( $childID ) . ' ">' . $child_cat->name . '</a>';
-
+                                
                                 echo '<ul class="post-title">';
-
+                                
                                 $query = new WP_Query( array( 'cat'=> $childID, 'posts_per_page'=>10 ) );
                                 while( $query->have_posts() ):$query->the_post();
                                 echo '<li><a href="'.get_the_permalink().'">'.get_the_title().'</a></li>';
-                                endwhile;
-                                wp_reset_postdata();
-
-                                echo '</ul>';
-
-                            }
+                            endwhile;
+                            wp_reset_postdata();
+                            
+                            echo '</ul>';
+                            
+                        }
                         echo '</ul></li>';
-                    } //end of categories logic 
-                ?>
-            </ul>
+                        } //end of categories logic 
+                    ?>
+                </ul>
+            </div>
         </div>
-
+        
         <div class="side-column">
             <div class="section-name">
-                    <h3>Side Stuff</h3>
+                <h3>Side Stuff</h3>
             </div>
              <!-- Structure: Side-Bar Widgets -->
                 <?php get_sidebar(); ?>
