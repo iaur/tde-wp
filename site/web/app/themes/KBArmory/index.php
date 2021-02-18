3e564e9fa53131ce49fd7e806ff9373d2dc3a720
Case <bernie.lu@caseMacBook-Pro.local>
2021-02-18 23:15:03 +0800

    redirect change for non www to www

--- site/web/app/themes/KBArmory/index.php
+++ site/web/app/themes/KBArmory/index.php
@@ -117,19 +117,18 @@ get_header(); ?>
                 </div>
              <!-- /Structure: Side-Bar Widgets -->
 
-             <div class="ads">
-                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-                    <!-- Exploit-Page-Vertical Ad Unit -->
-                    <ins class="adsbygoogle"
-                        style="display:block"
-                        data-ad-client="ca-pub-8248541475840368"
-                        data-ad-slot="2699305013"
-                        data-ad-format="auto"
-                        data-full-width-responsive="true"></ins>
-                    <script>
-                        (adsbygoogle = window.adsbygoogle || []).push({});
-                </script>
-             </div>
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                <!-- Exploit-Page-Vertical Ad Unit -->
+                <ins class="adsbygoogle"
+                    style="display:block"
+                    data-ad-client="ca-pub-8248541475840368"
+                    data-ad-slot="2699305013"
+                    data-ad-format="auto"
+                    data-full-width-responsive="true"></ins>
+                <script>
+                    (adsbygoogle = window.adsbygoogle || []).push({});
+            </script>
+
         </div>
     </div>
 <!-- /Structure: Site-Content -->
