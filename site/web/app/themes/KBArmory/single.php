b8f5609da3298483e214ea31b97d104a9a3b9c2f
Case <bernie.lu@caseMacBook-Pro.local>
2021-06-20 14:08:23 +0800

    v2 adjustment to ads space

--- site/web/app/themes/KBArmory/single.php
+++ site/web/app/themes/KBArmory/single.php
@@ -77,7 +77,11 @@ get_header(); ?>
                                     <h4>Boring Ads. Pardon me :)</h4>
                                 </div>
                                     
-                                    <div class="yearly-archive">
+                                    
+                                    <div class="adspace-ads-container">
+
+                                        <img src="http://thedailyexploits.local/app/uploads/2021/06/2021-06-20-at-1.57-PM.png" alt="test ad local">
+                                        
                                         <div class="square-ads">
                                             <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
                                             <!-- Square-Ad-Tree-Column -->
