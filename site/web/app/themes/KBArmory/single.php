e642783da16506392c431cff6b83abf20034fd4c
Case <bernie.lu@caseMacBook-Pro.local>
2021-06-26 23:30:24 +0800

    v6 adjustment to ads space

--- site/web/app/themes/KBArmory/single.php
+++ site/web/app/themes/KBArmory/single.php
@@ -81,30 +81,33 @@ get_header(); ?>
                                     <div class="adspace-ads-container">
 
                                         
-                                        
-                                    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-                                        <!-- Exploit-Page-Vertical Ad Unit -->
-                                        <ins class="adsbygoogle"
-                                            style="display:block"
-                                            data-ad-client="ca-pub-8248541475840368"
-                                            data-ad-slot="2699305013"
-                                            data-ad-format="auto"
-                                            data-full-width-responsive="true"></ins>
-                                        <script>
-                                            (adsbygoogle = window.adsbygoogle || []).push({});
-                                    </script>
+                                            
+                                        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                                            <!-- Exploit-Page-Vertical Ad Unit -->
+                                            <ins class="adsbygoogle"
+                                                style="display:block"
+                                                data-ad-client="ca-pub-8248541475840368"
+                                                data-ad-slot="2699305013"
+                                                data-ad-format="auto"
+                                                data-full-width-responsive="true"></ins>
+                                            <script>
+                                                (adsbygoogle = window.adsbygoogle || []).push({});
+                                        </script>
 
-                                    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-                                        <!-- Exploit-Page-Vertical Ad Unit -->
-                                        <ins class="adsbygoogle"
-                                            style="display:block"
-                                            data-ad-client="ca-pub-8248541475840368"
-                                            data-ad-slot="2699305013"
-                                            data-ad-format="auto"
-                                            data-full-width-responsive="true"></ins>
-                                        <script>
-                                            (adsbygoogle = window.adsbygoogle || []).push({});
-                                    </script>  
+                                        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                                            <!-- Exploit-Page-Vertical Ad Unit -->
+                                            <ins class="adsbygoogle"
+                                                style="display:block"
+                                                data-ad-client="ca-pub-8248541475840368"
+                                                data-ad-slot="2699305013"
+                                                data-ad-format="auto"
+                                                data-full-width-responsive="true"></ins>
+                                            <script>
+                                                (adsbygoogle = window.adsbygoogle || []).push({});
+                                        </script>  
+
+                                        <div class="adspace-adds-containter-child1"></div>
+                                        <div class="adspace-adds-containter-child2"></div>
                                     
                                     </div>
 
